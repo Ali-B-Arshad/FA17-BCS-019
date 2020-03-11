10ce43b6324c404cc6d9b4bcab1a55c1044f04f5
Ali Bin Arshad <ali@localhost.localdomain>
2020-03-11 23:20:21 +0500

    Updates

---
+++ precache-manifest.2ee07331f17a76e3d0238cc20998ccbf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51eadef11befe43253240c5861189ed2",
+    "url": "/FA17-BCS-019/index.html"
+  },
+  {
+    "revision": "34922a602486bd970aa4",
+    "url": "/FA17-BCS-019/static/css/main.1438434d.chunk.css"
+  },
+  {
+    "revision": "fc96c3621b027493570d",
+    "url": "/FA17-BCS-019/static/js/2.9b9e3b00.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/FA17-BCS-019/static/js/2.9b9e3b00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34922a602486bd970aa4",
+    "url": "/FA17-BCS-019/static/js/main.34e6b9d1.chunk.js"
+  },
+  {
+    "revision": "daff617d0fae830c8e77",
+    "url": "/FA17-BCS-019/static/js/runtime-main.539a0902.js"
+  },
+  {
+    "revision": "c904aa1945acbb7092e320fdb94e807c",
+    "url": "/FA17-BCS-019/static/media/messi.c904aa19.jpg"
+  }
+]);
